f53a2376c482ec0ef93a1ad7dde1312ad7a10d98
SickeningTrack7 <61024288+SickeningTrack7@users.noreply.github.com>
2020-02-13 13:23:41 -0500

    Create my.py

---
+++ my.py
@@ -0,0 +1,8 @@
+count = 1
+while(count < 4):
+    count_1 = 1
+    print("This is the outer loop,", count)
+    while(count_1 < 3):
+        print("This is the inner loop,", count_1)
+        count_1 += 1
+    count+=1
